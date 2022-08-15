5bdaad7e613f172dd3a0d8e333be710df6673482
xiayang <xiayang@ushareit.com>
2022-08-15 18:31:13 +0800

    feat: proxyRefs

--- src/reactivity/src/ref.ts
+++ src/reactivity/src/ref.ts
@@ -51,3 +51,19 @@ export function isRef(ref) {
 export function unRef(ref) {
     return isRef(ref) ? ref.value : ref;
 }
+
+export function proxyRefs(objectWithRefs) {
+    return new Proxy(objectWithRefs, {
+        get (target, key) {
+            return unRef(target[key]);
+        },
+        set (target, key, value) {
+            // 对象中的属性必须是 ref类型，且value 不能是 ref类型
+            if (isRef(target[key]) && !isRef(value)) {
+                return target[key].value = value;
+            } else {
+                return Reflect.set(target, key, value)
+            }
+        }
+    })
+}
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../src/effect"
 import { reactive } from "../src/reactive";
-import { isRef, ref, unRef } from "../src/ref";
+import { isRef, proxyRefs, ref, unRef } from "../src/ref";
 
 describe('ref', () => {
     it('happy path', () => {
@@ -59,5 +59,25 @@ describe('ref', () => {
 
         expect(unRef(a)).toBe(1);
         expect(unRef(1)).toBe(1);
+    })
+
+    it('proxyRefs', () => {
+        const user = {
+            age: ref(10),
+            name: 'xiaoming'
+        };
+
+        const proxyUser = proxyRefs(user);
+        expect(proxyUser.age).toBe(10);
+        expect(user.age.value).toBe(10);
+
+        // 如果 set 的值是非响应式，就是替换；如果是响应式，就是重新赋值
+        proxyUser.age = 20;
+        expect(proxyUser.age).toBe(20);
+        expect(user.age.value).toBe(20);
+
+        proxyUser.age = ref(10);
+        expect(proxyUser.age).toBe(10);
+        expect(user.age.value).toBe(10);
     })
 })
