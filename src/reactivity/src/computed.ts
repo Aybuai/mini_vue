b1d5b361cd643373b3ee8d1bfc14e794ed86fe82
Aybuai <13624448268@163.com>
2022-08-16 21:32:21 +0800

    update: format code

--- src/reactivity/src/computed.ts
+++ src/reactivity/src/computed.ts
@@ -1,29 +1,28 @@
 import { ReactiveEffect } from "./effect";
 
-class ComputedRefImpl{
+class ComputedRefImpl {
+  private _dirty: Boolean = true;
+  private _value: any;
+  private _effect: ReactiveEffect;
 
-    private _dirty: Boolean = true;
-    private _value: any;
-    private _effect: ReactiveEffect;
+  constructor(getter) {
+    this._effect = new ReactiveEffect(getter, () => {
+      // 修改响应式对象数据时，触发set，后执行 scheduler， 重新把计算属性放开，_dirty 变为 true
+      if (!this._dirty) {
+        this._dirty = true;
+      }
+    });
+  }
 
-    constructor(getter) {
-        this._effect = new ReactiveEffect(getter, () => {
-            // 修改响应式对象数据时，触发set，后执行 scheduler， 重新把计算属性放开，_dirty 变为 true
-            if (!this._dirty) {
-                this._dirty = true
-            }
-        });
+  get value() {
+    if (this._dirty) {
+      this._dirty = false;
+      this._value = this._effect.run();
     }
-
-    get value() {
-        if (this._dirty) {
-            this._dirty = false
-            this._value = this._effect.run();
-        }
-        return this._value
-    }
+    return this._value;
+  }
 }
 
 export function computed(getter) {
-    return new ComputedRefImpl(getter)
+  return new ComputedRefImpl(getter);
 }
--- src/reactivity/tests/computed.spec.ts
+++ src/reactivity/tests/computed.spec.ts
@@ -1,53 +1,53 @@
-import { computed } from "../src/computed"
-import { reactive } from "../src/reactive"
+import { computed } from "../src/computed";
+import { reactive } from "../src/reactive";
 
-describe('computed', () => {
-    it('happy path', () => {
-        // .value 拿到返回值
-        // 会缓存计算好的值
-        const user = reactive({
-            age: 1
-        })
+describe("computed", () => {
+  it("happy path", () => {
+    // .value 拿到返回值
+    // 会缓存计算好的值
+    const user = reactive({
+      age: 1,
+    });
 
-        const age = computed(() => {
-            return user.age
-        })
+    const age = computed(() => {
+      return user.age;
+    });
 
-        expect(age.value).toBe(1)
-    })
+    expect(age.value).toBe(1);
+  });
 
-    it('should compute lazily', () => {
-        const user = reactive({
-            age: 1
-        })
+  it("should compute lazily", () => {
+    const user = reactive({
+      age: 1,
+    });
 
-        const getter = jest.fn(() => {
-            return user.age
-        })
+    const getter = jest.fn(() => {
+      return user.age;
+    });
 
-        const dummy = computed(getter)
+    const dummy = computed(getter);
 
-        // lazy 未调用getter时，不会执行计算
-        expect(getter).not.toHaveBeenCalled()
+    // lazy 未调用getter时，不会执行计算
+    expect(getter).not.toHaveBeenCalled();
 
-        // 调用getter，执行计算
-        expect(dummy.value).toBe(1)
-        expect(getter).toHaveBeenCalledTimes(1)
+    // 调用getter，执行计算
+    expect(dummy.value).toBe(1);
+    expect(getter).toHaveBeenCalledTimes(1);
 
-        // 响应式对象未修改，再次执行，不会执行计算，从缓存中拿出之前计算的值
-        dummy.value;
-        expect(getter).toHaveBeenCalledTimes(1)
+    // 响应式对象未修改，再次执行，不会执行计算，从缓存中拿出之前计算的值
+    dummy.value;
+    expect(getter).toHaveBeenCalledTimes(1);
 
-        // // 响应式对象变更，且调用时再计算
-        user.age = 2 // 触发set -> trigger -> get
-        expect(getter).toHaveBeenCalledTimes(1)
+    // // 响应式对象变更，且调用时再计算
+    user.age = 2; // 触发set -> trigger -> get
+    expect(getter).toHaveBeenCalledTimes(1);
 
-        // // now it should compute
-        expect(dummy.value).toBe(2)
-        expect(getter).toHaveBeenCalledTimes(2)
+    // // now it should compute
+    expect(dummy.value).toBe(2);
+    expect(getter).toHaveBeenCalledTimes(2);
 
-        // should not compute again
-        dummy.value
-        expect(getter).toHaveBeenCalledTimes(2)
-    })
-})
+    // should not compute again
+    dummy.value;
+    expect(getter).toHaveBeenCalledTimes(2);
+  });
+});
