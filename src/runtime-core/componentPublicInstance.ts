093033d01b6788b055111efc8bcf47d9bb9642d7
xiayang <xiayang@ushareit.com>
2022-08-26 10:57:11 +0800

    fix: PublicInstanceProxyHandlers

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -8,15 +8,18 @@ const publicPropertiesMap = {
 export const PublicInstanceProxyHandlers = {
   get({ _: instance }, key) {
     // setupState
+    // 用户访问 proxy[key]
+    // 这里就匹配一下看看是否有对应的 function
+    // 有的话就直接调用这个 function
     const { setupState, props } = instance;
-    if (key in setupState) {
-      return setupState[key];
-    }
 
-    // 重构优化相同逻辑，判断对象是否存在属性，存在就返回value
+
     if (hasOwn(setupState, key)) {
+      // 先检测访问的 key 是否存在于 setupState 中, 是的话直接返回
       return setupState[key];
     } else if (hasOwn(props, key)) {
+      // 看看 key 是不是在 props 中
+      // 代理是可以访问到 props 中的 key 的
       return props[key];
     }
 
